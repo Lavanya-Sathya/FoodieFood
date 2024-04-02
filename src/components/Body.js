d945cacb1d74aa60759c03dbde328865952c3e0f
Lavanya <sathyalavanya5@gmail.com>
2024-04-02 23:16:32 +0530

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,11 +15,13 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await res.json();
+    // console.log(jsonData);
     const resData =
-      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+      jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
     setFilteredRestaurantList(resData);
     setRestaurantList(resData);
+    // console.log(filteredRestaurantList);
   };
 
   return RestaurantList.length === 0 ? (
